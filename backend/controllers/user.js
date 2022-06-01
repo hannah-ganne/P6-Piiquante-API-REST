9fbd3efd0f661d6e969636d0df7e72abafcf2e88
hannahganne <ganne.hannah@gmail.com>
2022-06-01 23:30:09 +0200

    add password validator

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,21 +2,43 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
+/**
+ * validate if password conditions are met
+ */
+const passwordValidator = require('password-validator');
 
+const passwordSchema = new passwordValidator();
+
+passwordSchema
+.is().min(8)
+.has().uppercase()
+.has().lowercase()
+.has().digits(3)
+
+/**
+ * user sign up
+ */
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
-        email: req.body.email,
-        password: hash
-        });
-        user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
+    if (passwordSchema.validate(req.body.password)) {
+        bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+            email: req.body.email,
+            password: hash
+            });
+            user.save()
+            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+            .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+    } else {
+        return res.status(401).json({ error: 'Mot de passe invalide !' })
+    }
 };
 
+/**
+ * user log in
+ */
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,23 +1,3 @@
-// const jwt = require('jsonwebtoken');
-
-// module.exports = (req, res, next) => {
-//     try {
-//         const token = req.headers.authorization.split(' ')[1];
-//         const decodedToken = jwt.verify(token, 'gksskwpfpaldpffldjtaortm');
-//         const userId = decodedToken.userId;
-//         req.auth = { userId };
-//         if (req.body.userId && req.body.userId !== userId) {
-//         throw "L'identifiant invalide !";
-//         } else {
-//         next();
-//         }
-//     } catch {
-//         res.status(401).json({
-//         error: new Error(' requête invalide !')
-//         });
-//     }
-//     };
-
 let jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
