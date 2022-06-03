3edca8a51fb0c71c2b9f7e1b1ed94e3f5415c524
hannahganne <ganne.hannah@gmail.com>
2022-06-03 22:14:03 +0200

    add password validator and express rate limit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const limiter = require('../middleware/rateLimiter');
+const passwordValidator = require('../middleware/password-validator');
 
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', passwordValidator, userCtrl.signup);
+router.post('/login', limiter.loginLimiter, userCtrl.login);
 
 module.exports = router;
