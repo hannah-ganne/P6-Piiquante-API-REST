63ad25425e997eb28bf6cc1158b0ab586e9d326e
hannahganne <ganne.hannah@gmail.com>
2022-06-04 22:40:49 +0200

    edit rate liimt

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
-    windowMs: 60 * 60 * 1000,
-    max: 1000,
+    windowMs: 10 * 60 * 1000,
+    max: 10,
     standardHeaders: true,
     legacyHeaders: false,
     message: "Trop de tentatives de connexion ont été effectuées à partir de cette IP, veuillez réessayer dans une heure"
