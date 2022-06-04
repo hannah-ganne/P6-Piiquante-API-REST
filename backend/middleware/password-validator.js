4bc829ab9cb214b9f145a7a6f1d2a5b47db2af4a
hannahganne <ganne.hannah@gmail.com>
2022-06-04 22:40:59 +0200

    delete comments

--- backend/middleware/password-validator.js
+++ backend/middleware/password-validator.js
@@ -3,13 +3,13 @@ const passwordValidator = require('password-validator');
 const passwordSchema = new passwordValidator();
 
 passwordSchema
-.is().min(8)                                    // Minimum length 8
-.is().max(100)                                  // Maximum length 100
-.has().uppercase()                              // Must have uppercase letters
-.has().lowercase()                              // Must have lowercase letters
-.has().digits(2)                                // Must have at least 2 digits
-.has().not().spaces()                           // Should not have spaces
-.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
+.is().min(8)
+.is().max(100)
+.has().uppercase()
+.has().lowercase()
+.has().digits(2)
+.has().not().spaces()
+.is().not().oneOf(['Passw0rd', 'Password123']);
 
 module.exports = (req, res, next) => {
     if (passwordSchema.validate(req.body.password)) {
